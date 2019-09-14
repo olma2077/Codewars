2efc7f419eb15360d99c41fd20b1b286e7bfc214
MathWay <ma7hway@gmail.com>
2019-09-14 12:29:40 +0900

    Create Reverse or rotate.go

---
+++ Reverse or rotate.go
@@ -0,0 +1,34 @@
+package kata
+
+func rotate(s1 string) string {
+  return s1[1:] + s1[:1]
+}
+
+func reverse(s1 string) string {
+  s2 := ""
+  for i := len(s1); i > 0; i-- {
+    s2 += string(s1[i-1])
+  }
+  return s2
+}
+
+
+func Revrot(s string, n int) string {
+    if len(s) < n || n == 0 {
+      return ""
+    }
+    result := ""
+    for i := 0; i < len(s) / n; i ++ {
+      subs := s[0+n*i:n*(i+1)]
+      y := 0
+      for _, x := range subs {
+        y += int((x-48)*(x-48)*(x-48))
+      }
+      if y % 2 == 0 {
+        result += reverse(subs)
+      } else {
+        result += rotate(subs)
+      }
+    }
+    return result
+}
